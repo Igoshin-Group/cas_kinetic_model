912e926f40cffbac2148905093e2c0684a143b7a
Andrew D. Hecht <46638934+adhecht@users.noreply.github.com>
2025-01-29 19:40:03 -0600

    Update generate_parameter_swap_figure.m
    Remove redundant code and update paths

--- matlab/model_implementation/generate_parameter_swap_figure.m
+++ matlab/model_implementation/generate_parameter_swap_figure.m
@@ -2,12 +2,9 @@ clc;
 clear;
 close all;
 
-working_directory = pwd(); % get the script directory
-switch_to_project_directory(); % switch to the code directory
-
 %% Load the fits
 % Load the distal substrate indices (off_idx)
-load("../data/matlab/distal_substrate_indices.mat");
+load("./data/matlab/distal_substrate_indices.mat");
 substrates = off_idx;
 num_substrates = length(off_idx) + 1; % add 1 for the on-target
 
@@ -31,80 +28,6 @@ rank = ranks(rank_index);
 model = models{rank_index};
 psets_on = model(:,1); model(:,1) = [];
 psets_off = model;
-
-% %% Load the best fit: on-target
-% % Load the best fit
-% folder = "../outputs/Model-02G/On-Target-Parameter-Dynamic-Weights/sub_48/";
-% [fits, scores] = load_parameter_table(folder);
-% 
-% % find the minimum score
-% [min_score, idx] = min(scores);
-% fprintf("Index of Best Fit: %i\n", idx);
-% 
-% % get the best parameter set
-% best_fit = fits(idx,:);
-% 
-% C = 1; % [Cas9] (nM)
-% [model, on_target_energies] = generate_model_from_energies(best_fit, C);
-% 
-% on_target_model = model(:,1);
-% num_substrates = 2;
-% num_variants = 2;
-
-% %% Load distal substrate indices
-% % load and pre-process data for fitting
-% threshold = inf;
-% dataset = load_distal_data(threshold);
-% 
-% substrates.wt = [];
-% substrates.hf1 = [];
-% substrates.enh = [];
-% 
-% off_idx = [];
-% 
-% % add dissociation constants (from Chen et al. 2017, see Figure 1b)
-% dataset.wt(1).dissociation_constant = 14.23;
-% dataset.hf1(1).dissociation_constant = 17.80;
-% dataset.enh(1).dissociation_constant = 12.70;
-% 
-% for ii = 1:length(dataset.wt)
-%     
-%     % extract current substrates
-%     substrate_wt = dataset.wt(ii);
-%     substrate_hf1 = dataset.hf1(ii);
-%     substrate_enh = dataset.enh(ii);
-%     
-%     % compute effective dissociation constant
-%     substrate_wt.dissociation_constant = substrate_wt.normalized_binding * dataset.wt(1).dissociation_constant;
-%     substrate_hf1.dissociation_constant = substrate_hf1.normalized_binding * dataset.hf1(1).dissociation_constant;
-%     substrate_enh.dissociation_constant = substrate_enh.normalized_binding * dataset.enh(1).dissociation_constant;
-%     
-%     % get the mismatch pattern, and extract the distal mismatches
-%     mismatch_pattern = substrate_wt.mismatch_pattern;
-%     mismatch_pattern = mismatch_pattern{1};
-%     
-%     distal = substrate_wt.distal_mismatches;
-%     proximal = substrate_wt.proximal_mismatches;
-%     
-%     % look for the correct mismatch patterns
-%     if proximal == 0
-%         if substrate_wt.distal_mismatches == 0
-%             continue; %skip for the purposes of making the table
-%         end
-%         if isempty(substrates.wt)
-%             substrates.wt = substrate_wt;
-%             substrates.hf1 = substrate_hf1;
-%             substrates.enh = substrate_enh;
-%         else
-%             substrates.wt(end+1) = substrate_wt;
-%             substrates.hf1(end+1) = substrate_hf1;
-%             substrates.enh(end+1) = substrate_enh;
-%         end
-%         off_idx(end+1) = ii;
-%     else
-%         continue;
-%     end
-% end
 
 %% Load dataset
 % load and pre-process data for fitting
@@ -142,41 +65,6 @@ for ii = 1:num_variants
 end
 
 dataset = [on_target_data', off_target_data]; % should be equivalent to the fitted dataset
-
-
-
-
-% %% Load the best fit
-% psets_off = [];
-% folder_base = "../outputs/Model-02G/Off-Target-Parameter-Dynamic-Weights/";
-% num_substrates = length(off_idx)+1; % add one for the on-target
-% best_scores = zeros(length(off_idx),1);
-% for ii = 1:length(off_idx)
-%     folder = folder_base + "sub_" + num2str(off_idx(ii)) + "/";
-%     
-%     [fits, scores] = load_parameter_table(folder);
-% 
-%     % find the minimum score
-%     [min_score, idx] = min(scores);
-% 
-%     % get the best parameter set
-%     best_fit = fits(idx,:);
-%     best_scores(ii) = min_score;
-%     
-%     C = 1; % [Cas9] (nM)
-%     [model, energies] = generate_off_target_model_from_energies(best_fit, on_target_energies, C);
-%     psets_off_cur = model(:,2);
-%     
-%     if isempty(psets_off)
-%         psets_off = psets_off_cur;
-%     else
-%         psets_off(:,end+1) = psets_off_cur;
-%     end
-% end
-% 
-% psets = [on_target_model, psets_off];
-% 
-% psets_on = on_target_model;
 
 pset_on_wt = psets_on(1);
 pset_on_hf1 = psets_on(2);
@@ -320,6 +208,7 @@ X = [2, 2, 2, 2; 3, 3, 3, 3; 4, 4, 4, 4; 5, 5, 5, 5] - 1;
 Y = mean_errors;
 CI = sigma_errors;
 
+figure();
 barplot(X,Y,CI);
 
 xlabel("Distal Mismatches (#)");
@@ -328,10 +217,4 @@ title("Effects of Conformational Transitions on Error");
 legend("WT-Cas9 (native)", "WT HNH / HF1 R-Loop", "HF1 HNH / WT R-Loop", "HF1-Cas9 (native)");
 set(gca,"YScale","log");
 pbaspect([0.5,1,1]);
-save_figure(gcf, "../figures/", "pred/", "parameter-swap-boxplot-ci-log-small-x");
-
-%% Local Functions
-function err = compute_error_from_mfpt(pset_on, pset_off)
-disp("Not actually computing errors from MFPTs");
-err = compute_forward_error(pset_off);
-end
+save_figure(gcf, "../../figures/", "pred/", "wt-hf1-parameter-swap");
